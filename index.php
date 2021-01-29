ddf5dcf3a09e3a2faf12621947558b65a85e9f6a
Jessica Sestak <jsestak2@mail.greenriver.edu>
2021-01-29 15:18:56 -0500

    Added config file and controller page

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+//this is my controller
+
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+//require the autoload file
+require_once('vendor/autoload.php');
+
+//create an instance of the base class
+$f3 = Base::instance();
+$f3->set('DEBUG', 3);
+
+//default root
+$f3->route('GET /', function(){
+    echo "My Pets";
+}
+);
+
+//run fat free
+$f3->run();
